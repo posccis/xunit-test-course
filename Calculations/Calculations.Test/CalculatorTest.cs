b07e9c40279dfc3eed47efca67a5c20cfefb1261
Posccis <lkzq33@gmail.com>
2022-11-15 22:30:37 -0300

    update(main)= finished the course

--- Calculations/Calculations.Test/CalculatorTest.cs
+++ Calculations/Calculations.Test/CalculatorTest.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Calculations.Tests
 {
-    public class CalculatorTest
+    public class CalculatorFixture 
     {
+        public Calculator calc => new Calculator();
+    }
+    public class CalculatorTest : IClassFixture<CalculatorFixture>
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly CalculatorFixture _calculatorFixture;
+        private readonly MemoryStream memoryStream;
+        public CalculatorTest(ITestOutputHelper testOutputHelper, CalculatorFixture calculatorFixture)
+        {
+            _testOutputHelper = testOutputHelper;
+            _calculatorFixture = calculatorFixture;
+            memoryStream = new MemoryStream();
+        }
         [Fact]
         public void Add_GivenTwoIntValues_ReturnsInt() 
         {
-            Calculator calculator = new Calculator();
+            Calculator calculator = _calculatorFixture.calc ;
             int result = calculator.Add(1, 2);
             Assert.Equal(3, result);
         }
@@ -18,21 +33,46 @@ namespace Calculations.Tests
         [Fact]
         public void AddDouble_GivenTwoDoubleValues_ReturnsDouble() 
         {
-            Calculator calculator = new Calculator();
+            Calculator calculator = _calculatorFixture.calc;
             double result = calculator.AddDouble(1.2, 3.5);
             Assert.Equal(4.7, result);
         }
         [Fact]
+        [Trait("Category", "Fibo")]
         public void FiboDoesNotIncludeZero() 
         {
-            Calculator calculator = new Calculator();
+            Calculator calculator = _calculatorFixture.calc;
             Assert.All(calculator.FiboNumbers, n => Assert.NotEqual(0, n));
         }
         [Fact]
+        [Trait("Category", "Fibo")]
         public void FiboIncludes13() 
         {
-            Calculator calculator = new Calculator();
+            Calculator calculator = _calculatorFixture.calc;
             Assert.Contains(13, calculator.FiboNumbers);
+        }
+        [Fact]
+        public void IsOdd_GivenOddValue_ReturnsTrue() 
+        {
+            Calculator calculator = _calculatorFixture.calc;
+            bool result = calculator.IsOdd(1);
+            Assert.True(result);
+        }
+        [Fact]
+        public void IsOdd_GivenEvenValue_ReturnsFalse()
+        {
+            Calculator calculator = _calculatorFixture.calc;
+            bool result = calculator.IsOdd(2);
+            Assert.False(result);
+        }
+        [Theory]
+        [InlineData(1, true)]
+        [InlineData(200, false)]
+        public void IsOdd_TestOddAndEven(int value, bool expected) 
+        {
+            Calculator calculator = _calculatorFixture.calc;
+            bool result = calculator.IsOdd(value);
+            Assert.Equal(expected,result);
         }
     }
 }
--- Calculations/Calculations/Calculator.cs
+++ Calculations/Calculations/Calculator.cs
@@ -16,5 +16,10 @@ namespace Calculations
         }
 
         public List<int> FiboNumbers => new List<int> { 1, 1, 2, 3, 5, 8, 13 };
+
+        public bool IsOdd(int value) 
+        {
+            return (value % 2 )== 1;
+        }
     }
 }
